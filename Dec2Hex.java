703c4814fe9159736a2804a30236f6f3f5d180d0
Christopher Lindsey <cllindseybusiness@gmail.com>
2017-05-30 17:50:03 -0400

    Imported current working build of the software
    - Current build relies on client's attention to instructions for successful use
    - Next update will account for any and all input errors
    - .jar file included in upload for public use

---
+++ Dec2Hex.java
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<jsmoothproject>
+<JVMSearchPath>registry</JVMSearchPath>
+<JVMSearchPath>javahome</JVMSearchPath>
+<JVMSearchPath>jrepath</JVMSearchPath>
+<JVMSearchPath>jdkpath</JVMSearchPath>
+<JVMSearchPath>exepath</JVMSearchPath>
+<JVMSearchPath>jview</JVMSearchPath>
+<arguments></arguments>
+<classPath>Dec2Hex.java</classPath>
+<currentDirectory>${EXECUTABLEPATH}</currentDirectory>
+<embeddedJar>false</embeddedJar>
+<executableName>Dec2Hex.exe</executableName>
+<iconLocation>Hex.png</iconLocation>
+<initialMemoryHeap>-1</initialMemoryHeap>
+<mainClassName></mainClassName>
+<maximumMemoryHeap>-1</maximumMemoryHeap>
+<maximumVersion></maximumVersion>
+<minimumVersion></minimumVersion>
+<skeletonName>WinService Wrapper</skeletonName>
+<skeletonProperties>
+<key>ServiceName</key>
+<value>myservice</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>ServiceDisplayName</key>
+<value>My Service</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>Message</key>
+<value>This program needs Java to run. Please download it at http://www.java.comf</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>Autostart</key>
+<value>1</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>Logfile</key>
+<value>service.log</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>Interactive</key>
+<value>0</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>JniSmooth</key>
+<value>0</value>
+</skeletonProperties>
+<skeletonProperties>
+<key>Debug</key>
+<value>0</value>
+</skeletonProperties>
+</jsmoothproject>
